0d7fabd4917175ff6a02946062ad0bd5617b1762
zcl1183 <1183802549@qq.com>
2023-04-10 11:53:30 +0800

    配置@绝对路径

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service")
+//先确定node_modules项目中是否有path模块， 如果没有path模块需要先安装path; npm install path --save
+const path = require("path")
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+        //"@crud": resolve("src/components/Crud"),
+      },
+    },
+  },
 })
