5e3ad6bbfb34cd6bbcabf5017be9a84eea214245
zcl1183 <1183802549@qq.com>
2023-04-10 11:33:21 +0800

    测试scss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="home1">
+      <img alt="Vue logo" src="../assets/logo.png" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import { defineComponent } from "vue"
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+  export default defineComponent({
+    name: "HomeView",
+  })
 </script>
+<style lang="scss">
+  .home {
+    .home1 {
+      background: red;
+    }
+  }
+</style>
